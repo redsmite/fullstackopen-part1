98f27b498a22450dfb1871a009e935889794a206
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-04-26 22:19:05 +0800

    create props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,18 +4,20 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='George' />
+      <Hello name='Daisy' />
     </div>
   )
 }
