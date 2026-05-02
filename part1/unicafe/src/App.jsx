cbfb89599c618df3b2ef7d799ee1f2df8a795b32
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-05-02 17:51:30 +0800

    saved unicafe exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,39 +1,83 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-const Button = ({onClick, text}) => {
-  return (
-    <button onClick = {onClick}>{text}</button>
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>{props.text}</button>
   )
 }
 
-const App = () =>{
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
+const Statistics = ({ feedback }) => {
+  if (feedback.total === 0)  {return (<p>No feedback given</p>)} else { 
+    return (
+      <div>
+        <table>
+          <tbody>
+            <StatisticsLine text="good" value={feedback.good} />
+            <StatisticsLine text="neutral" value={feedback.neutral} />
+            <StatisticsLine text="bad" value={feedback.bad} />
+            <StatisticsLine text="all" value={feedback.total} />
+            <StatisticsLine text="average" value={feedback.total === 0 ? 0 : feedback.score / feedback.total} />
+            <StatisticsLine text="positive" value={feedback.total === 0 ? 0 : (feedback.good / feedback.total) * 100 + '%'} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+const StatisticsLine = (props) => {
+  return(
+    <tr><td>{props.text}</td><td>{props.value}</td></tr>
+  )
+}
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    score: 0,
+    total: 0,
   })
 
-  const handleLeftClicks = () => {
+  const handleGoodClick = () => {
     const newClick = {
-      ...clicks,
-      left: clicks.left + 1
+      ...feedback,
+      good: feedback.good + 1,
+      score: feedback.score + 1,
+      total: feedback.total + 1,
     }
-    return setClicks(newClick)
+    setFeedback(newClick);
+  }
+  
+  const handleNeutralClick = () => {
+    const newClick = {
+      ...feedback,
+      neutral: feedback.neutral + 1,
+      total: feedback.total + 1,
+    }
+    setFeedback(newClick);
+  } 
+  
+  const handleBadClick = () => {
+    const newClick = {
+      ...feedback,
+      bad: feedback.bad + 1,
+      score: feedback.score - 1,
+      total: feedback.total + 1,
+    }
+    setFeedback(newClick);
   }
 
-  const handleRightClicks = () => {
-    const newClick = {
-      ...clicks,
-      right: clicks.right + 1
-    }
-    return setClicks(newClick)
-  }
-
-  return (
+  return(
     <>
-      <h1>Left Counter: {clicks.left}</h1>
-      <h1>Right Counter: {clicks.right}</h1>
-      <Button onClick={handleLeftClicks} text="Left Click"/>
-      <Button onClick={handleRightClicks} text="Right Click"/>  
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodClick} text="good"/>
+      <Button onClick={handleNeutralClick} text="neutral"/>
+      <Button onClick={handleBadClick} text="bad"/>
+
+      <h1>statistics</h1>
+      <Statistics feedback={feedback} />
     </>
   )
 }
